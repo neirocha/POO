2bdab4ccd686d5be3bc929052db163e9f50ddc3c
neirocha <saturncomunicacao@hotmail.com>
2015-04-30 20:07:58 -0300

    Os Primeiros arquivos e classes do projeto, primeira avaliacao

---
+++ src/Clientes/Arrayclientes.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nei Rocha
+ * Date: 29/04/2015
+ * Time: 18:27
+ */
+
+require_once "Cliente.php";
+
+class Arrayclientes extends \Clientes\Cliente
+{
+    $cliente1 = new Cliente();
+    $cliente2 = new Cliente();
+    $cliente3 = new Cliente();
+    $cliente4 = new Cliente();
+    $cliente5 = new Cliente();
+    $cliente6 = new Cliente();
+    $cliente7 = new Cliente();
+    $cliente8 = new Cliente();
+    $cliente9 = new Cliente();
+    $cliente10 = new Cliente();
+
+    $cliente1->setId(1)->setNome("Nei Rocha")->setCpf("825.356.158-25")->setEndereco("Av. São Rafael")->setTelefone("3393-4885")->setEmail("saturncomunicacao@hotmail.com");
+    $cliente2->setId(2)->setNome("Nei Rocha")->setCpf("825.356.158-25")->setEndereco("Av. São Rafael")->setTelefone("3393-4885")->setEmail("saturncomunicacao@hotmail.com");
+    $cliente3->setId(3)->setNome("Nei Rocha")->setCpf("825.356.158-25")->setEndereco("Av. São Rafael")->setTelefone("3393-4885")->setEmail("saturncomunicacao@hotmail.com");
+    $cliente4->setId(4)->setNome("Nei Rocha")->setCpf("825.356.158-25")->setEndereco("Av. São Rafael")->setTelefone("3393-4885")->setEmail("saturncomunicacao@hotmail.com");
+    $cliente5->setId(5)->setNome("Nei Rocha")->setCpf("825.356.158-25")->setEndereco("Av. São Rafael")->setTelefone("3393-4885")->setEmail("saturncomunicacao@hotmail.com");
+    $cliente6->setId(6)->setNome("Nei Rocha")->setCpf("825.356.158-25")->setEndereco("Av. São Rafael")->setTelefone("3393-4885")->setEmail("saturncomunicacao@hotmail.com");
+    $cliente7->setId(7)->setNome("Nei Rocha")->setCpf("825.356.158-25")->setEndereco("Av. São Rafael")->setTelefone("3393-4885")->setEmail("saturncomunicacao@hotmail.com");
+    $cliente8->setId(8)->setNome("Nei Rocha")->setCpf("825.356.158-25")->setEndereco("Av. São Rafael")->setTelefone("3393-4885")->setEmail("saturncomunicacao@hotmail.com");
+    $cliente9->setId(9)->setNome("Nei Rocha")->setCpf("825.356.158-25")->setEndereco("Av. São Rafael")->setTelefone("3393-4885")->setEmail("saturncomunicacao@hotmail.com");
+    $cliente10->setId(10)->setNome("Nei Rocha")->setCpf("825.356.158-25")->setEndereco("Av. São Rafael")->setTelefone("3393-4885")->setEmail("saturncomunicacao@hotmail.com");
+
+
+
+}
+
---
+++ src/Clientes/Cliente.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nei Rocha
+ * Date: 29/04/2015
+ * Time: 17:44
+ */
+
+class Cliente
+{
+
+    protected $id;
+    protected $nome;
+    protected $cpf;
+    protected $endereco;
+    protected $telefone;
+    protected $email;
+
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setCpf($cpf)
+    {
+        $this->cpf = $cpf;
+    }
+
+    public function getCpf()
+    {
+        return $this->cpf;
+    }
+
+    public function setEndereco($endereco)
+    {
+        $this->endereco = $endereco;
+    }
+
+    public function getEndereco()
+    {
+        return $this->endereco;
+    }
+
+    public function setTelefone($telefone)
+    {
+        $this->telefone = $telefone;
+    }
+
+    public function getTelefone()
+    {
+        return $this->telefone;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+}
